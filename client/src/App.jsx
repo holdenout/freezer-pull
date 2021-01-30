04a749e969fbfa59e46f8678af4fb9b0532bec10
Holden <holdensheftel@gmail.com>
2021-01-29 23:32:36 -0700

    Display food list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import FoodList from './FoodList';
 
 function App() {
   return (
     <div className="App">
-      Placeholder
+      <FoodList />
     </div>
   );
 }
---
+++ client/src/FoodList.jsx
@@ -0,0 +1,22 @@
+import sampleFoodData from './sampleFoodData.json';
+import './FoodList.css';
+
+export default function FoodList() {
+  // Sort food by category then name
+  const foodData = sampleFoodData.items.sort(function(a, b) {
+    return a.category === b.category
+      ? a.name.localeCompare(b.name)
+      : a.category.localeCompare(b.category)
+  });
+  const foodItems = foodData.map((food) => {
+    return <li key={food.id} >{food.name}</li>;
+  });
+
+  return (
+    <div className="FoodList">
+      <ul>
+        {foodItems}
+      </ul>
+    </div>
+  );
+}
