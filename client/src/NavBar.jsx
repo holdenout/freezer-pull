b5bc92902c1f369c6b87cc80a45265fd0f8bfe02
Holden <holdensheftel@gmail.com>
2021-01-31 00:18:07 -0700

    Add placeholders and scrolling to navbar

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -3,7 +3,7 @@ import './NavBar.css';
 export default function NavBar() {
   return (
     <div className="navbar">
-      Placholder
+      Placholder1 Placholder2 Placholder3 Placholder4 Placholder5
     </div>
   )
 }
