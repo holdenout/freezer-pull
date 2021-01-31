27027929ac49cfed2cad7139cd30363e3115d8b6
Holden <holdensheftel@gmail.com>
2021-01-31 02:23:39 -0700

    Put food items in collapsibles

--- client/src/FoodList.jsx
+++ client/src/FoodList.jsx
@@ -1,15 +1,33 @@
+import {useState} from 'react';
 import sampleFoodData from './sampleFoodData.json';
+import Collapsible from './Collapsible.jsx';
 import './FoodList.css';
 
 export default function FoodList() {
+  const [isOpen, setOpen] = useState(null);
+
+  function toggleOpen(a) {
+    setOpen(isOpen === a ? null : a);
+  }
+
   // Sort food by category then name
   const foodData = sampleFoodData.items.sort(function(a, b) {
     return a.category === b.category
       ? a.name.localeCompare(b.name)
       : a.category.localeCompare(b.category)
   });
+
   const foodItems = foodData.map((food) => {
-    return <li key={food.id} >{food.name}</li>;
+    return (
+      <li key={food.id} >
+        <Collapsible
+          open={isOpen === food.id}
+          header={food.name}
+          content={`${food.name} content here.`}
+          toggleOpen={() => toggleOpen(food.id)}
+        />
+      </li>
+    );
   });
 
   return (
