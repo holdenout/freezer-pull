cf28fceec5f8487b9a1c4c5dc06a9967db2eee4b
Holden <holdensheftel@gmail.com>
2021-02-10 21:06:37 -0700

    Add styling

--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 import {Link} from "react-router-dom";
+
 export const LandingPage = ({toCarryover}) => {
   return (
     <div>
       <p>LandingPage under development</p>
-      <Link to="/carryover">Begin carryover count</Link>
+      <Link className="next" to="/carryover">Begin carryover count</Link>
     </div>
   );
 };
