845642bcffd04fd056933d3eb3a427cadfc3dcbb
Holden <holdensheftel@gmail.com>
2021-02-11 17:09:09 -0700

    Modify FoodList for generic useStore

--- client/src/components/FoodList.jsx
+++ client/src/components/FoodList.jsx
@@ -3,9 +3,9 @@ import {useStore} from "./useStore.js";
 import Collapsible from "./Collapsible.jsx";
 import "./FoodList.css";
 
-export const FoodList = ({content, children}) => {
+export const FoodList = ({content, propToUpdate, children}) => {
   const [isOpen, setOpen] = useState(null);
-  const [foodData, updateState] = useStore("carryover");
+  const [foodData, updateState] = useStore(propToUpdate);
 
   const toggleOpen = (id) => {
     setOpen(isOpen === id ? null : id);
