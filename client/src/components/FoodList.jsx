6dcf3d7d9b66f7e5b65ec9fd8d7c300d9761eba7
Holden <holdensheftel@gmail.com>
2021-02-18 19:06:47 -0700

    Add search bar

--- client/src/components/FoodList.jsx
+++ client/src/components/FoodList.jsx
@@ -4,27 +4,40 @@ import "./FoodList.css";
 
 export const FoodList = ({foodData, content, children}) => {
   const [isOpen, setOpen] = useState(null);
+  const [search, setSearch] = useState("");
+  const [filteredFoodData, setFilteredFoodData] = useState(foodData);
 
   const toggleOpen = (id) => {
     setOpen(isOpen === id ? null : id);
   };
 
-  const foodItems = foodData.map((food) => {
-    return (
-      <li key={food.id}>
-        <Collapsible
-          open={isOpen === food.id}
-          header={food.name}
-          content={content({food, isOpen})}
-          toggleOpen={() => toggleOpen(food.id)}
-        />
-      </li>
-    );
-  });
+  const handleSearch = ({target: {value: newSearch}}) => {
+    setSearch(newSearch);
+    setFilteredFoodData(foodData.filter((food) => {
+      return food.name.toLowerCase().includes(newSearch.toLowerCase());
+    }));
+  };
 
   return (
     <div className="food-list">
-      <ul className="food-items">{foodItems}</ul>
+      <div className="search">
+        <input type="text" placeholder="Search" value={search} onChange={handleSearch} />
+        <span className="clear">&#x2715;</span>
+      </div>
+      <ul className="food-items">
+        {filteredFoodData.map((food) => {
+          return (
+            <li key={food.id}>
+              <Collapsible
+                open={isOpen === food.id}
+                header={food.name}
+                content={content({food, isOpen})}
+                toggleOpen={() => toggleOpen(food.id)}
+              />
+            </li>
+          );
+        })}
+      </ul>
       {children}
     </div>
   );
