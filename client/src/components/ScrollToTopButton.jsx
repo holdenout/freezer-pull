1cd83aba73b38922f22f700f66b32367e736423e
Holden <holdensheftel@gmail.com>
2021-02-19 18:58:16 -0700

    Change button visibility on scroll

--- client/src/components/ScrollToTopButton.jsx
+++ client/src/components/ScrollToTopButton.jsx
@@ -2,15 +2,29 @@ import {useState, useEffect} from "react";
 import "./ScrollToTopButton.css";
 
 export const ScrollToTopButton = () => {
-  const [visible, setVisible] = useState();
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    if (window.scrollY === 0) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisible);
+    return () => window.removeEventListener("scroll", toggleVisible);
+  }, []);
 
   const handleClick = () => {
-    window.scrollTo({top: 0, behavior: "smooth"})
-  }
+    window.scrollTo({top: 0, behavior: "smooth"});
+  };
 
   return (
-    <div className="scroll-button" onClick={handleClick}>
-      <div className="triangle-up"/>
-    </div>
+    visible &&
+      <div className="scroll-button" onClick={handleClick}>
+        <div className="triangle-up" />
+      </div>
   );
 };
