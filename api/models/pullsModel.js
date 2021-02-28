6e52ae1852aa7bfaf10efb47f1939a09eac41f3c
Holden <holdensheftel@gmail.com>
2021-02-28 02:36:27 -0700

    Add pulls model

---
+++ api/models/pullsModel.js
@@ -0,0 +1,13 @@
+const db = require("./db.js");
+
+const Pull = {};
+
+// create
+Pull.create = (req, res) => {
+  db.query("INSERT INTO pulls (pull_DATE) VALUE (TIMESTAMP(NOW()))", (err, data) => {
+    if (err) throw err;
+    res.status(200).send(data);
+  })
+}
+
+module.exports = Pull;
