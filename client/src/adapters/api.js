54255665c68bdc0143bdafc5e66166bf1c6ea274
Holden <holdensheftel@gmail.com>
2021-03-02 15:27:38 -0700

    Update header type

--- client/src/adapters/api.js
+++ client/src/adapters/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 export const foodApi = axios.create({
-  baseURL: "http://localhost:4000/food"
+  baseURL: "http://localhost:4000/food",
+  headers: {"Content-type": "application/json"}
 });
 
 export const pullApi = axios.create({
-  baseURL: "http://localhost:4000/pull"
+  baseURL: "http://localhost:4000/pull",
+  headers: {"Content-type": "application/json"}
 });
