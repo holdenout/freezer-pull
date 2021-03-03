a801b59a5f6d11ec2aa4a8b71e30cacc6a78ab35
Holden <holdensheftel@gmail.com>
2021-03-02 17:09:34 -0700

    Add semicolons to sql query

--- api/models/pullsModel.js
+++ api/models/pullsModel.js
@@ -4,7 +4,7 @@ const Pull = {};
 
 // create
 Pull.create = (req, res) => {
-  db.query("INSERT INTO pulls (pull_DATE) VALUE (TIMESTAMP(NOW()))", (err, data) => {
+  db.query("INSERT INTO pulls (pull_DATE) VALUE (TIMESTAMP(NOW()));", (err, data) => {
     if (err) throw err;
     res.status(200).send(data);
   })
