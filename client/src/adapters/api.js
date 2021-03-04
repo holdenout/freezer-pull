c697ff14abe2c6f43b0ddc4fd9ead785c5348d62
Holden <holdensheftel@gmail.com>
2021-03-03 23:24:05 -0700

    Code styling

--- client/src/adapters/api.js
+++ client/src/adapters/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export const foodApi = axios.create({
   baseURL: "http://localhost:4000/food",
-  headers: {"Content-type": "application/json"}
+  headers: {"Content-type": "application/json"},
 });
 
 export const pullApi = axios.create({
   baseURL: "http://localhost:4000/pull",
-  headers: {"Content-type": "application/json"}
+  headers: {"Content-type": "application/json"},
 });
--- client/src/adapters/foodAdapter.js
+++ client/src/adapters/foodAdapter.js
@@ -2,7 +2,7 @@ import {foodApi as api} from "./api.js";
 
 export const foodAdapter = {
   getAll: () => api.get("/all"),
-  getById: (id) => api.get(`/${id}`)
+  getById: (id) => api.get(`/${id}`),
 };
 
 export default foodAdapter;
