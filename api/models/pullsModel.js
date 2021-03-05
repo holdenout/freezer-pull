329ecebb7b2b9d473ef3c66462fbccbe550cc371
Holden <holdensheftel@gmail.com>
2021-03-05 16:53:20 -0700

    Add food_item_pull insertions

--- api/models/pullsModel.js
+++ api/models/pullsModel.js
@@ -6,8 +6,20 @@ const Pull = {};
 Pull.create = (req, res) => {
   db.query("INSERT INTO pulls (pull_DATE) VALUE (TIMESTAMP(NOW()));", (err, data) => {
     if (err) throw err;
-    res.status(200).send(data);
-  })
+
+    // get id of pull
+    const {insertId: pull_id} = data;
+
+    // create nested area of insert values
+    const pullData = req.body.pullData.map(({sku, carryover, pull}) => {
+      return [pull_id, sku, carryover, pull];
+    });
+
+    db.query("INSERT INTO food_item_pull VALUES ?", [pullData], (err, data) => {
+      if (err) throw err;
+      res.status(201).send(data);
+    });
+  });
 }
 
 module.exports = Pull;
