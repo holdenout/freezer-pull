42d27d32293fe87e6c30d3ed67358b99f5e90a6e
Holden <holdensheftel@gmail.com>
2021-03-17 21:46:14 -0600

    Verify username as valid

---
+++ api/middleware/verifySignUp.js
@@ -0,0 +1,39 @@
+const db = require("../models/db.js");
+const User = require("../models/userModel.js");
+
+const validateFormat = (req, res, next) => {
+  const newUsername = req.body.username;
+
+  const isValid = newUsername.search(/^[A-Za-z]\w*$/) > -1;
+
+  if (!isValid) {
+    res.status(400).send({
+      message: "Invalid characters",
+    });
+    return;
+  }
+
+  next();
+};
+
+const checkDuplicateUsername = (req, res, next) => {
+  const newUsername = req.body.username;
+
+  User.getByName(newUsername, (err, data) => {
+    if (!err) {
+      res.status(400).send({
+        message: "Username already exists.",
+      });
+      return;
+    }
+
+    next();
+  });
+};
+
+const verifySignup = {
+  isValid: isValid,
+  checkDuplicateUsername: checkDuplicateUsername,
+};
+
+module.exports = verifySignup;
