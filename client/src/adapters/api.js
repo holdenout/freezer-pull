6b3f1f7f846f0fbf3bfe0e8dab678702015d4690
Holden <holdensheftel@gmail.com>
2021-04-03 13:17:54 -0600

    Prep for different baseURL in production

--- client/src/adapters/api.js
+++ client/src/adapters/api.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
+const baseURL = process.env.NODE_ENV === "production" ? "http://localhost:4000" : "http://localhost:4000"
+
 export const foodApi = axios.create({
-  baseURL: "http://localhost:4000/food",
+  baseURL: `${baseURL}/food`,
   headers: {"Content-type": "application/json"},
 });
 
 export const pullApi = axios.create({
-  baseURL: "http://localhost:4000/pull",
+  baseURL: `${baseURL}/pull`,
   headers: {"Content-type": "application/json"},
 });
 
 export const authApi = axios.create({
-  baseURL: "http://localhost:4000/auth",
+  baseURL: `${baseURL}/auth`,
   headers: {"Content-type": "application/json"},
 });
