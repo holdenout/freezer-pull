a3fdc25c5a9cc51a4f863a410df3994538ee07a9
Holden <holdensheftel@gmail.com>
2021-04-04 02:59:01 -0600

    Correct landing page text

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,7 +3,10 @@ import {Link} from "react-router-dom";
 export const LandingPage = ({toCarryover}) => {
   return (
     <div>
-      <p>LandingPage under development</p>
+      <p>
+        Currently logged in as{" "}
+        {JSON.parse(localStorage.getItem("user")).username}
+      </p>
       <Link className="btn submit-btn next-btn" to="/carryover">
         Begin carryover count
       </Link>
