0c8f5f36526e033626ca9080d4fb26b63585b4fb
Holden <holdensheftel@gmail.com>
2021-04-06 12:45:34 -0600

    Display warning on non-mobile devices

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,11 +1,20 @@
 import {Link} from "react-router-dom";
 
 export const LandingPage = ({toCarryover}) => {
+  // Hacky way of checking for non-mobile platform
+  // https://coderwall.com/p/i817wa/one-line-function-to-detect-mobile-devices-with-javascript
+  const isNotMobile = (typeof window.orientation === "undefined") || (navigator.userAgent.indexOf("IEMobile") === -1);
+
   return (
     <div>
+      {isNotMobile && (
+        <p>
+          This application is best viewed on a mobile platform.
+        </p>
+      )}
       <p>
         Currently logged in as{" "}
-        {JSON.parse(localStorage.getItem("user")).username}
+        {JSON.parse(localStorage.getItem("user")).username}.
       </p>
       <Link className="btn submit-btn next-btn" to="/carryover">
         Begin carryover count
