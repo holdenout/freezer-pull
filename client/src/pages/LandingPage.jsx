4024fa2686da172482f91da5b59321427bb36228
Holden <holdensheftel@gmail.com>
2021-04-23 14:53:19 -0600

    Correct mobile warning check

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,17 +1,11 @@
 import {Link} from "react-router-dom";
 
 export const LandingPage = ({toCarryover}) => {
-  // Hacky way of checking for non-mobile platform
-  // https://coderwall.com/p/i817wa/one-line-function-to-detect-mobile-devices-with-javascript
-  const isNotMobile =
-    typeof window.orientation === "undefined" ||
-    navigator.userAgent.indexOf("IEMobile") === -1;
-
   return (
     <div>
-      {isNotMobile && (
-        <p>This application is best viewed on a mobile platform.</p>
-      )}
+      <p className="mobile-warning">
+        This application is best viewed on a mobile platform.
+      </p>
       <p>
         Currently logged in as{" "}
         {JSON.parse(localStorage.getItem("user")).username}.
